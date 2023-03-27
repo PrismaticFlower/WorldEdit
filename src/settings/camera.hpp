bfc7488c63c66155cdbe6693068963a3889045d1
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2023-03-27 13:39:54 +1300

    Update camera.hpp

--- src/settings/camera.hpp
+++ src/settings/camera.hpp
@@ -6,7 +6,7 @@ namespace we::settings {
 
 struct camera {
    float move_speed = 20.0f;
-   float look_sensitivity = 0.007f;
+   float look_sensitivity = 0.003f;
    float pan_sensitivity = 0.075f;
    float sprint_power = 1.5f;
 };
