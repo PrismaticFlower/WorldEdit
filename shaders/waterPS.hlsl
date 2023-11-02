4df4dd8941218cfb4bb22f32dfcfce7c998690e8
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2023-11-03 10:04:24 +1300

    fix water shader texture sampling

--- shaders/waterPS.hlsl
+++ shaders/waterPS.hlsl
@@ -6,12 +6,12 @@
 
 struct input_vertex
 {
-   float2 positionWS : POSITIONWS;
+   float3 positionWS : POSITIONWS;
 };
 
 float4 main(input_vertex input) : SV_TARGET
 {
    Texture2D<float4> color_map = ResourceDescriptorHeap[cb_water.color_map_index];
 
-   return cb_water.color * color_map.Sample(sampler_anisotropic_wrap, input.positionWS);
+   return cb_water.color * color_map.Sample(sampler_anisotropic_wrap, input.positionWS.xz);
 }
