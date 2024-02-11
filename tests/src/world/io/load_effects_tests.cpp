f71edf2a68bc03a0e7c9afb39ed50394414d44a9
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-02-11 15:26:20 +1300

    update formatting

--- tests/src/world/io/load_effects_tests.cpp
+++ tests/src/world/io/load_effects_tests.cpp
@@ -49,59 +49,41 @@ Effect("ColorControl")
    color_control loaded = load_effects(world_fx_color_control, output).color_control;
 
    CHECK(not loaded.enable_per_platform);
-
    CHECK(loaded.enable_pc);
 
    CHECK(loaded.gamma_brightness_per_platform);
-
    CHECK(loaded.gamma_brightness_pc == 0.6f);
-
    CHECK(loaded.gamma_brightness_xbox == 0.48f);
 
    CHECK(loaded.gamma_color_balance_per_platform);
-
    CHECK(loaded.gamma_color_balance_pc == 0.55f);
-
    CHECK(loaded.gamma_color_balance_xbox == 0.5f);
 
    CHECK(loaded.gamma_contrast_per_platform);
-
    CHECK(loaded.gamma_contrast_pc == 0.7f);
-
    CHECK(loaded.gamma_contrast_xbox == 0.54f);
 
    CHECK(loaded.gamma_correction_per_platform);
-
    CHECK(loaded.gamma_correction_pc == 0.5f);
+   CHECK(loaded.gamma_correction_xbox == 0.52f);
 
    CHECK(loaded.gamma_hue_per_platform);
-
    CHECK(loaded.gamma_hue_pc == 0.0f);
-
    CHECK(loaded.gamma_hue_xbox == 0.5f);
 
    CHECK(loaded.world_brightness_per_platform);
-
    CHECK(loaded.world_brightness_pc == 0.46f);
-
    CHECK(loaded.world_brightness_ps2 == 0.55f);
-
    CHECK(loaded.world_brightness_xbox == 0.46f);
 
    CHECK(loaded.world_contrast_per_platform);
-
    CHECK(loaded.world_contrast_pc == 0.48f);
-
    CHECK(loaded.world_contrast_ps2 == 0.40f);
-
    CHECK(loaded.world_contrast_xbox == 0.48f);
 
    CHECK(loaded.world_saturation_per_platform);
-
    CHECK(loaded.world_saturation_pc == 0.65f);
-
    CHECK(loaded.world_saturation_ps2 == 0.75f);
-
    CHECK(loaded.world_saturation_xbox == 0.65f);
 }
 
@@ -126,39 +108,30 @@ Effect("FogCloud")
    fog_cloud loaded = load_effects(world_fx, output).fog_cloud;
 
    CHECK(not loaded.enable_per_platform);
-
    CHECK(loaded.enable_pc);
 
    CHECK(not loaded.texture_per_platform);
-
    CHECK(loaded.texture_pc == "cloud");
 
    CHECK(not loaded.range_per_platform);
-
    CHECK(loaded.range_pc == float2{35.0f, 110.0f});
 
    CHECK(not loaded.color_per_platform);
-
    CHECK(loaded.color_pc == float4{168.0f, 172.0f, 180.0f, 128.0f} / 255.0f);
 
    CHECK(not loaded.velocity_per_platform);
-
    CHECK(loaded.velocity_pc == float2{5.0f, 0.0f});
 
    CHECK(not loaded.rotation_per_platform);
-
    CHECK(loaded.rotation_pc == 0.1f);
 
    CHECK(not loaded.height_per_platform);
-
    CHECK(loaded.height_pc == 24.0f);
 
    CHECK(not loaded.particle_size_per_platform);
-
    CHECK(loaded.particle_size_pc == 32.0f);
 
    CHECK(not loaded.particle_density_per_platform);
-
    CHECK(loaded.particle_density_pc == 100.0f);
 }
 
@@ -178,19 +151,15 @@ Effect("Wind")
    wind loaded = load_effects(world_fx, output).wind;
 
    CHECK(not loaded.enable_per_platform);
-
    CHECK(loaded.enable_pc);
 
    CHECK(not loaded.velocity_per_platform);
-
    CHECK(loaded.velocity_pc == float2{3.0f, 0.6f});
 
    CHECK(not loaded.velocity_range_per_platform);
-
    CHECK(loaded.velocity_range_pc == 0.75f);
 
    CHECK(not loaded.velocity_change_rate_per_platform);
-
    CHECK(loaded.velocity_change_rate_pc == 0.2f);
 }
 
@@ -238,69 +207,53 @@ Effect("Precipitation")
    precipitation loaded = load_effects(world_fx, output).precipitation;
 
    CHECK(not loaded.enable_per_platform);
-
    CHECK(loaded.enable_pc);
 
    CHECK(not loaded.type_per_platform);
-
    CHECK(loaded.type_pc == precipitation_type::streaks);
 
    CHECK(not loaded.range_per_platform);
-
    CHECK(loaded.range_pc == 16.0f);
 
    CHECK(not loaded.color_per_platform);
-
    CHECK(loaded.color_pc == float3{200.0f, 200.0f, 228.0f} / 255.0f);
 
    CHECK(not loaded.velocity_range_per_platform);
-
    CHECK(loaded.velocity_range_pc == 0.6f);
 
    CHECK(not loaded.particle_density_range_per_platform);
-
    CHECK(loaded.particle_density_range_pc == 0.1f);
 
    CHECK(not loaded.camera_cross_velocity_scale_per_platform);
-
    CHECK(loaded.camera_cross_velocity_scale_pc == 0.3f);
 
    CHECK(not loaded.camera_axial_velocity_scale_per_platform);
-
    CHECK(loaded.camera_axial_velocity_scale_pc == 0.9f);
 
    CHECK(not loaded.ground_effect_per_platform);
-
    CHECK(loaded.ground_effect_pc == "huge_splash");
 
    CHECK(not loaded.ground_effect_spread_per_platform);
-
    CHECK(loaded.ground_effect_spread_pc == 16);
 
    CHECK(not loaded.particle_density_per_platform);
-
    CHECK(loaded.particle_density_pc == 80.0f);
 
    CHECK(not loaded.velocity_per_platform);
-
    CHECK(loaded.velocity_pc == 4.0f);
 
    CHECK(not loaded.streak_length_per_platform);
-
    CHECK(loaded.streak_length_pc == 1.7f);
 
    CHECK(not loaded.ground_effects_per_sec_per_platform);
-
    CHECK(loaded.ground_effects_per_sec_pc == 7);
 
    CHECK(loaded.alpha_min_max_per_platform);
-
    CHECK(loaded.alpha_min_max_pc == float2{0.3f, 0.45f});
    CHECK(loaded.alpha_min_max_ps2 == float2{0.8f, 1.0f});
    CHECK(loaded.alpha_min_max_xbox == float2{0.2f, 0.3f});
 
    CHECK(loaded.particle_size_per_platform);
-
    CHECK(loaded.particle_size_pc == 0.02f);
    CHECK(loaded.particle_size_ps2 == 0.06f);
    CHECK(loaded.particle_size_xbox == 0.03f);
@@ -321,15 +274,12 @@ Effect("Precipitation")
    precipitation loaded = load_effects(world_fx, output).precipitation;
 
    CHECK(not loaded.type_per_platform);
-
    CHECK(loaded.type_pc == precipitation_type::quads);
 
    CHECK(not loaded.texture_per_platform);
-
    CHECK(loaded.texture_pc == "fx_ember");
 
    CHECK(not loaded.rotation_range_per_platform);
-
    CHECK(loaded.rotation_range_pc == 25.0f);
 }
 
@@ -357,51 +307,39 @@ Effect("Lightning")
    lightning loaded = load_effects(world_fx, output).lightning;
 
    CHECK(not loaded.enable_per_platform);
-
    CHECK(loaded.enable_pc);
 
    CHECK(not loaded.color_per_platform);
-
    CHECK(loaded.color_pc == float3{220.0f, 220.0f, 255.0f} / 255.0f);
 
    CHECK(not loaded.sunlight_fade_factor_per_platform);
-
    CHECK(loaded.sunlight_fade_factor_pc == 0.2f);
 
    CHECK(not loaded.sky_dome_darken_factor_per_platform);
-
    CHECK(loaded.sky_dome_darken_factor_pc == 0.3f);
 
    CHECK(not loaded.brightness_min_per_platform);
-
    CHECK(loaded.brightness_min_pc == 0.7f);
 
    CHECK(not loaded.fade_time_per_platform);
-
    CHECK(loaded.fade_time_pc == 0.3f);
 
    CHECK(not loaded.time_between_flashes_min_max_per_platform);
-
    CHECK(loaded.time_between_flashes_min_max_pc == float2{2.0f, 4.0f});
 
    CHECK(not loaded.time_between_sub_flashes_min_max_per_platform);
-
    CHECK(loaded.time_between_sub_flashes_min_max_pc == float2{0.02f, 0.7f});
 
    CHECK(not loaded.num_sub_flashes_min_max_per_platform);
-
    CHECK(loaded.num_sub_flashes_min_max_pc == std::array{1, 8});
 
    CHECK(not loaded.horizon_angle_min_max_per_platform);
-
    CHECK(loaded.horizon_angle_min_max_pc == std::array{20, 80});
 
    CHECK(not loaded.sound_crack_per_platform);
-
    CHECK(loaded.sound_crack_pc == "test_amb_thunder");
 
    CHECK(not loaded.sound_sub_crack_per_platform);
-
    CHECK(loaded.sound_sub_crack_pc == "test_amb_thundersub");
 }
 
