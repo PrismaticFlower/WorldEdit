b2fc43d0f583b27d3ffba53d8914194621d34efe
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-03-05 13:42:15 +1300

    add no coalesce set_value tests

--- tests/src/edits/set_value_tests.cpp
+++ tests/src/edits/set_value_tests.cpp
@@ -360,4 +360,101 @@ TEST_CASE("edits set_creation_path_node_location coalesce", "[Edits]")
    REQUIRE(edit_context.euler_rotation == float3{0.0f, 0.0f, 0.0f});
 }
 
+TEST_CASE("edits set_memory_value no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   auto edit =
+      make_set_memory_value(&world.global_lights.env_map_texture, "starfield"s);
+   auto other_edit =
+      make_set_memory_value(&world.global_lights.global_light_1, "mountains"s);
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
+TEST_CASE("edits set_vector_value no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   auto edit = make_set_vector_value(&world.paths[0].nodes, 0,
+                                     &world::path::node::position,
+                                     float3{1.0f, 1.0f, 1.0f});
+   auto other_edit = make_set_vector_value(&world.paths[0].nodes, 1,
+                                           &world::path::node::position,
+                                           float3{2.0f, 2.0f, 2.0f});
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
+TEST_CASE("edits set_vector_value alt constructor no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   auto edit =
+      make_set_vector_value(&world.sectors[0].points, 0, float2{16.0f, 16.0f});
+   auto other_edit =
+      make_set_vector_value(&world.sectors[0].points, 1, float2{32.0f, 32.0f});
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
+TEST_CASE("edits set_multi_value2 no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   interaction_targets.creation_entity = world::portal{.width = 1.0f, .height = 2.0f};
+
+   world::portal& portal = interaction_targets.creation_entity.get<world::portal>();
+
+   auto edit = make_set_multi_value(&portal.width, 2.0f, &portal.height, 4.0f);
+   auto other_edit =
+      make_set_multi_value(&world.lights[0].range, 8.0f, &portal.height, 16.0f);
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
+TEST_CASE("edits set_multi_value3 no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   interaction_targets.creation_entity = world::barrier{.size = {0.0f, 0.0f}};
+
+   world::barrier& barrier =
+      interaction_targets.creation_entity.get<world::barrier>();
+
+   auto edit = make_set_multi_value(&barrier.rotation_angle, 1.0f,
+                                    &barrier.position, float3{1.0f, 1.0f, 1.0f},
+                                    &barrier.size, float2{2.0f, 2.0f});
+   auto other_edit = make_set_multi_value(&barrier.rotation_angle, 2.0f,
+                                          &world.planning_hubs[0].position,
+                                          float3{2.0f, 2.0f, 2.0f},
+                                          &barrier.size, float2{4.0f, 4.0f});
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
+TEST_CASE("edits make_set_path_node_property_value no coalesce", "[Edits]")
+{
+   world::world world = test_world;
+   world::interaction_targets interaction_targets;
+   world::edit_context edit_context{world, interaction_targets.creation_entity};
+
+   auto edit =
+      make_set_path_node_property_value(&world.paths[0].nodes, 0, 0, "NewValue");
+   auto other_edit =
+      make_set_path_node_property_value(&world.paths[0].nodes, 0, 1, "NewerValue");
+
+   REQUIRE(not edit->is_coalescable(*other_edit));
+}
+
 }
