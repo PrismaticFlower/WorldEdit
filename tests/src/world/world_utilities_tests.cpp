807acacaf232f67619e4f5f2cabe83fda37e3dd8
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-04-18 18:52:56 +1200

    fix find_closest_edge test

--- tests/src/world/world_utilities_tests.cpp
+++ tests/src/world/world_utilities_tests.cpp
@@ -125,7 +125,7 @@ TEST_CASE("world utilities find_closest_node", "[World][Utilities]")
    REQUIRE(find_closest_node({0.0f, 1.1f, 0.0f}, path).next_is_forward);
 }
 
-TEST_CASE("world utilities find_closest_point", "[World][Utilities]")
+TEST_CASE("world utilities find_closest_edge", "[World][Utilities]")
 {
    sector sector{.points = {
                     {0.0f, 0.0f},
@@ -134,11 +134,8 @@ TEST_CASE("world utilities find_closest_point", "[World][Utilities]")
                     {0.0f, 4.0f},
                  }};
 
-   REQUIRE(find_closest_point({0.0f, 0.9f}, sector).index == 1);
-   REQUIRE(not find_closest_point({0.0f, 0.9f}, sector).next_is_forward);
-
-   REQUIRE(find_closest_point({0.0f, 1.1f}, sector).index == 1);
-   REQUIRE(find_closest_point({0.0f, 1.1f}, sector).next_is_forward);
+   REQUIRE(find_closest_edge({0.0f, 0.9f}, sector) == 0);
+   REQUIRE(find_closest_edge({0.0f, 1.1f}, sector) == 1);
 }
 
 }
