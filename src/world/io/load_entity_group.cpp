e0598d5c934b31e1b24d9f94488af91d3fff5a16
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-09-10 09:41:23 +1200

    refactor load_entity_group_from_string

--- src/world/io/load_entity_group.cpp
+++ src/world/io/load_entity_group.cpp
@@ -34,6 +34,151 @@ auto read_position(const assets::config::node& node) -> float3
            -node.values.get<float>(2)};
 }
 
+void read_object(const assets::config::node& node, entity_group& group_out)
+{
+   object& object = group_out.objects.emplace_back();
+
+   object.name = node.values.get<std::string>(0);
+   object.class_name = lowercase_string{node.values.get<std::string>(1)};
+
+   for (auto& obj_prop : node) {
+      if (string::iequals(obj_prop.key, "ChildRotation"sv)) {
+         object.rotation = read_rotation(obj_prop);
+      }
+      else if (string::iequals(obj_prop.key, "ChildPosition"sv)) {
+         object.position = read_position(obj_prop);
+      }
+      else if (string::iequals(obj_prop.key, "Team"sv)) {
+         object.team = obj_prop.values.get<int>(0);
+      }
+      else if (string::iequals(obj_prop.key, "Layer"sv) or
+               string::iequals(obj_prop.key, "SeqNo"sv) or
+               string::iequals(obj_prop.key, "NetworkId"sv) or
+               string::iequals(obj_prop.key, "GeometryFile"sv)) {
+         continue;
+      }
+      else {
+         object.instance_properties.push_back(
+            {.key = obj_prop.key, .value = obj_prop.values.get<std::string>(0)});
+      }
+   }
+}
+
+void read_light(const assets::config::node& node, entity_group& group_out,
+                output_stream& output)
+{
+   light& light = group_out.lights.emplace_back();
+
+   light.name = node.values.get<std::string>(0);
+
+   for (auto& light_prop : node) {
+      if (string::iequals(light_prop.key, "Rotation"sv)) {
+         light.rotation = read_rotation(light_prop);
+      }
+      else if (string::iequals(light_prop.key, "Position"sv)) {
+         light.position = read_position(light_prop);
+      }
+      else if (string::iequals(light_prop.key, "Type"sv)) {
+         switch (const int type = light_prop.values.get<int>(0); type) {
+         case 1:
+         case 2:
+         case 3:
+         case 4:
+         case 5:
+         case 6:
+            light.light_type = static_cast<light_type>(type);
+            break;
+         default:
+            output.write("Warning! World light '{}' has invalid light type! "
+                         "Defaulting to point light.\n",
+                         light.name);
+            light.light_type = light_type::point;
+         }
+      }
+      else if (string::iequals(light_prop.key, "Color"sv)) {
+         light.color = {light_prop.values.get<float>(0),
+                        light_prop.values.get<float>(1),
+                        light_prop.values.get<float>(2)};
+      }
+      else if (string::iequals(light_prop.key, "Static"sv)) {
+         light.static_ = true;
+      }
+      else if (string::iequals(light_prop.key, "CastShadow"sv)) {
+         light.shadow_caster = true;
+      }
+      else if (string::iequals(light_prop.key, "CastSpecular"sv)) {
+         light.specular_caster = true;
+      }
+      else if (string::iequals(light_prop.key, "Texture"sv)) {
+         light.texture = light_prop.values.get<std::string>(0);
+
+         if (light_prop.values.size() >= 2) {
+            switch (const int addressing = light_prop.values.get<int>(1); addressing) {
+            case 0:
+            case 1:
+               light.texture_addressing = static_cast<texture_addressing>(addressing);
+               break;
+            default:
+               output.write("Warning! World light '{}' has invalid texture "
+                            "addressing mode! Defaulting to clamp.\n",
+                            light.name);
+               light.texture_addressing = texture_addressing::clamp;
+            }
+         }
+         else {
+            output.write("Warning! World light '{}' has no texture "
+                         "addressing mode! Defaulting to clamp.\n",
+                         light.name);
+            light.texture_addressing = texture_addressing::clamp;
+         }
+      }
+      else if (string::iequals(light_prop.key, "TileUV"sv)) {
+         light.directional_texture_tiling = {light_prop.values.get<float>(0),
+                                             light_prop.values.get<float>(1)};
+      }
+      else if (string::iequals(light_prop.key, "OffsetUV"sv)) {
+         light.directional_texture_offset = {light_prop.values.get<float>(0),
+                                             light_prop.values.get<float>(1)};
+      }
+      else if (string::iequals(light_prop.key, "Range"sv)) {
+         light.range = light_prop.values.get<float>(0);
+      }
+      else if (string::iequals(light_prop.key, "Cone"sv)) {
+         light.inner_cone_angle = light_prop.values.get<float>(0);
+         light.outer_cone_angle = light_prop.values.get<float>(1);
+      }
+      else if (string::iequals(light_prop.key, "Bidirectional"sv)) {
+         light.bidirectional = light_prop.values.get<int>(0) != 0;
+      }
+      else if (string::iequals(light_prop.key, "PS2BlendMode"sv)) {
+         switch (int mode = light_prop.values.get<int>(0); mode) {
+         case 0:
+         case 1:
+         case 2:
+            light.ps2_blend_mode = static_cast<ps2_blend_mode>(mode);
+            break;
+         default:
+            output
+               .write("Warning! World light '{}' has invalid PS2 Blend Mode! "
+                      "Defaulting to add.\n",
+                      light.name);
+            light.ps2_blend_mode = ps2_blend_mode::add;
+         }
+      }
+      else if (string::iequals(light_prop.key, "RegionName"sv)) {
+         light.region_name = light_prop.values.get<std::string>(0);
+      }
+      else if (string::iequals(light_prop.key, "RegionRotation"sv)) {
+         light.region_rotation = read_rotation(light_prop);
+      }
+      else if (string::iequals(light_prop.key, "RegionSize"sv)) {
+         light.region_size = {light_prop.values.get<float>(0),
+                              light_prop.values.get<float>(1),
+                              light_prop.values.get<float>(2)};
+      }
+   }
+}
+
 }
 
 auto load_entity_group_from_string(const std::string_view entity_group_data,
@@ -45,150 +190,10 @@ auto load_entity_group_from_string(const std::string_view entity_group_data,
 
       for (auto& key_node : assets::config::read_config(entity_group_data)) {
          if (string::iequals(key_node.key, "Object"sv)) {
-            object& object = group.objects.emplace_back();
-
-            object.name = key_node.values.get<std::string>(0);
-            object.class_name =
-               lowercase_string{key_node.values.get<std::string>(1)};
-
-            for (auto& obj_prop : key_node) {
-               if (string::iequals(obj_prop.key, "ChildRotation"sv)) {
-                  object.rotation = read_rotation(obj_prop);
-               }
-               else if (string::iequals(obj_prop.key, "ChildPosition"sv)) {
-                  object.position = read_position(obj_prop);
-               }
-               else if (string::iequals(obj_prop.key, "Team"sv)) {
-                  object.team = obj_prop.values.get<int>(0);
-               }
-               else if (string::iequals(obj_prop.key, "Layer"sv) or
-                        string::iequals(obj_prop.key, "SeqNo"sv) or
-                        string::iequals(obj_prop.key, "NetworkId"sv) or
-                        string::iequals(obj_prop.key, "GeometryFile"sv)) {
-                  continue;
-               }
-               else {
-                  object.instance_properties.push_back(
-                     {.key = obj_prop.key,
-                      .value = obj_prop.values.get<std::string>(0)});
-               }
-            }
+            read_object(key_node, group);
          }
          else if (string::iequals(key_node.key, "Light"sv)) {
-            light& light = group.lights.emplace_back();
-
-            light.name = key_node.values.get<std::string>(0);
-
-            for (auto& light_prop : key_node) {
-               if (string::iequals(light_prop.key, "Rotation"sv)) {
-                  light.rotation = read_rotation(light_prop);
-               }
-               else if (string::iequals(light_prop.key, "Position"sv)) {
-                  light.position = read_position(light_prop);
-               }
-               else if (string::iequals(light_prop.key, "Type"sv)) {
-                  switch (const int type = light_prop.values.get<int>(0); type) {
-                  case 1:
-                  case 2:
-                  case 3:
-                  case 4:
-                  case 5:
-                  case 6:
-                     light.light_type = static_cast<light_type>(type);
-                     break;
-                  default:
-                     output.write(
-                        "Warning! World light '{}' has invalid light type! "
-                        "Defaulting to point light.\n",
-                        light.name);
-                     light.light_type = light_type::point;
-                  }
-               }
-               else if (string::iequals(light_prop.key, "Color"sv)) {
-                  light.color = {light_prop.values.get<float>(0),
-                                 light_prop.values.get<float>(1),
-                                 light_prop.values.get<float>(2)};
-               }
-               else if (string::iequals(light_prop.key, "Static"sv)) {
-                  light.static_ = true;
-               }
-               else if (string::iequals(light_prop.key, "CastShadow"sv)) {
-                  light.shadow_caster = true;
-               }
-               else if (string::iequals(light_prop.key, "CastSpecular"sv)) {
-                  light.specular_caster = true;
-               }
-               else if (string::iequals(light_prop.key, "Texture"sv)) {
-                  light.texture = light_prop.values.get<std::string>(0);
-
-                  if (light_prop.values.size() >= 2) {
-                     switch (const int addressing = light_prop.values.get<int>(1);
-                             addressing) {
-                     case 0:
-                     case 1:
-                        light.texture_addressing =
-                           static_cast<texture_addressing>(addressing);
-                        break;
-                     default:
-                        output.write(
-                           "Warning! World light '{}' has invalid texture "
-                           "addressing mode! Defaulting to clamp.\n",
-                           light.name);
-                        light.texture_addressing = texture_addressing::clamp;
-                     }
-                  }
-                  else {
-                     output.write("Warning! World light '{}' has no texture "
-                                  "addressing mode! Defaulting to clamp.\n",
-                                  light.name);
-                     light.texture_addressing = texture_addressing::clamp;
-                  }
-               }
-               else if (string::iequals(light_prop.key, "TileUV"sv)) {
-                  light.directional_texture_tiling = {light_prop.values.get<float>(0),
-                                                      light_prop.values.get<float>(1)};
-               }
-               else if (string::iequals(light_prop.key, "OffsetUV"sv)) {
-                  light.directional_texture_offset = {light_prop.values.get<float>(0),
-                                                      light_prop.values.get<float>(1)};
-               }
-               else if (string::iequals(light_prop.key, "Range"sv)) {
-                  light.range = light_prop.values.get<float>(0);
-               }
-               else if (string::iequals(light_prop.key, "Cone"sv)) {
-                  light.inner_cone_angle = light_prop.values.get<float>(0);
-                  light.outer_cone_angle = light_prop.values.get<float>(1);
-               }
-               else if (string::iequals(light_prop.key, "Bidirectional"sv)) {
-                  light.bidirectional = light_prop.values.get<int>(0) != 0;
-               }
-               else if (string::iequals(light_prop.key, "PS2BlendMode"sv)) {
-                  switch (int mode = light_prop.values.get<int>(0); mode) {
-                  case 0:
-                  case 1:
-                  case 2:
-                     light.ps2_blend_mode = static_cast<ps2_blend_mode>(mode);
-                     break;
-                  default:
-                     output.write(
-                        "Warning! World light '{}' has invalid PS2 Blend Mode! "
-                        "Defaulting to add.\n",
-                        light.name);
-                     light.ps2_blend_mode = ps2_blend_mode::add;
-                  }
-               }
-               else if (string::iequals(light_prop.key, "RegionName"sv)) {
-                  light.region_name = light_prop.values.get<std::string>(0);
-               }
-               else if (string::iequals(light_prop.key, "RegionRotation"sv)) {
-                  light.region_rotation = read_rotation(light_prop);
-               }
-               else if (string::iequals(light_prop.key, "RegionSize"sv)) {
-                  light.region_size = {light_prop.values.get<float>(0),
-                                       light_prop.values.get<float>(1),
-                                       light_prop.values.get<float>(2)};
-               }
-            }
+            read_light(key_node, group, output);
          }
       }
 
