8d9e491d4822ee7377cecd4b61155284d7f7e557
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-11-21 18:18:43 +1300

    remove old translate gizmo

--- src/gizmo.cpp
+++ src/gizmo.cpp
@@ -55,9 +55,6 @@ bool gizmo::want_capture_mouse() const noexcept
    case mode::inactive: {
       return false;
    } break;
-   case mode::translate: {
-      return _translate.active_axis != axis::none;
-   } break;
    case mode::rotate: {
       return _rotate.active_axis != axis::none;
    } break;
@@ -77,88 +74,11 @@ void gizmo::update(const graphics::camera_ray cursor_ray,
       _mode = mode::inactive;
    }
 
-   _translating_last_frame = _translate.translating;
    _rotating_last_frame = _rotate.rotating;
 
    switch (_mode) {
    case mode::inactive: {
-      _translate = {};
-   } break;
-   case mode::translate: {
-      if (not _translate.translating) {
-         const float3 local_ray_origin = conjugate(_gizmo_rotation) * offset_ray_origin;
-         const float3 local_ray_direction = conjugate(_gizmo_rotation) * ray_direction;
-
-         const float half_length = _translate_gizmo_length / 2.0f;
-         const float padding = _translate_gizmo_hit_pad;
-
-         const float x_hit =
-            boxIntersection(local_ray_origin - float3{half_length, 0.0f, 0.0f},
-                            local_ray_direction, {half_length, padding, padding});
-         const float y_hit =
-            boxIntersection(local_ray_origin - float3{0.0f, half_length, 0.0f},
-                            local_ray_direction, {padding, half_length, padding});
-         const float z_hit =
-            boxIntersection(local_ray_origin - float3{0.0f, 0.0f, half_length},
-                            local_ray_direction, {padding, padding, half_length});
-
-         _translate.active_axis = axis::none;
-
-         float nearest = std::numeric_limits<float>::max();
-
-         if (x_hit > 0.0f and x_hit < nearest) {
-            nearest = x_hit;
-            _translate.active_axis = axis::x;
-         }
-         if (y_hit > 0.0f and y_hit < nearest) {
-            nearest = y_hit;
-            _translate.active_axis = axis::y;
-         }
-         if (z_hit > 0.0f and z_hit < nearest) {
-            nearest = z_hit;
-            _translate.active_axis = axis::z;
-         }
-
-         if (_translate.active_axis == axis::none and is_mouse_down) {
-            _translate.mouse_down_outside_gizmo = true;
-         }
-         else if (is_mouse_down and not _translate.mouse_down_outside_gizmo) {
-            _translate.start_position = _gizmo_position;
-            _translate.current_cursor_position =
-               get_translate_position(cursor_ray, camera.position(), _gizmo_position);
-            _translate.translating = true;
-         }
-         else if (not is_mouse_down) {
-            _translate.mouse_down_outside_gizmo = false;
-         }
-
-         if (_translate.mouse_down_outside_gizmo) {
-            _translate.active_axis = axis::none;
-         }
-      }
-
-      if (_translate.translating and is_mouse_down and
-          _translate.active_axis != axis::none) {
-         const float3 position =
-            get_translate_position(cursor_ray, camera.position(),
-                                   _translate.current_cursor_position);
-         const float3 movement = position - _translate.current_cursor_position;
-
-         if (_translate.active_axis == axis::x) {
-            _translate.movement_x = movement.x;
-         }
-         else if (_translate.active_axis == axis::y) {
-            _translate.movement_y = movement.y;
-         }
-         else if (_translate.active_axis == axis::z) {
-            _translate.movement_z = movement.z;
-         }
-
-         _translate.current_cursor_position = position;
-      }
-      else if (_translate.translating) {
-         _translate = {};
-      }
+      _rotate = {};
    } break;
    case mode::rotate: {
       graphics::camera_ray rayGS{
@@ -266,8 +186,6 @@ void gizmo::update_scale(const graphics::camera& camera, const float gizmo_scale
    const float camera_scale = distance(camera.position(), _gizmo_position) *
                               camera.projection_matrix()[0].x;
 
-   _translate_gizmo_length = translate_gizmo_length * camera_scale * gizmo_scale;
-   _translate_gizmo_hit_pad = translate_gizmo_hit_pad * camera_scale * gizmo_scale;
    _rotate_gizmo_radius = rotate_gizmo_radius * camera_scale * gizmo_scale;
    _rotate_gizmo_hit_pad = rotate_gizmo_hit_pad * camera_scale * gizmo_scale;
    _axis_line_length = camera.far_clip();
@@ -275,48 +193,7 @@ void gizmo::update_scale(const graphics::camera& camera, const float gizmo_scale
 
 void gizmo::draw(world::tool_visualizers& tool_visualizers) noexcept
 {
-   if (_mode == mode::translate) {
-      const float length = _translate_gizmo_length;
-
-      const bool x_hover = _translate.active_axis == axis::x;
-      const bool y_hover = _translate.active_axis == axis::y;
-      const bool z_hover = _translate.active_axis == axis::z;
-
-      const float3 x_axis = _gizmo_rotation * float3{1.0f, 0.0f, 0.0f};
-      const float3 y_axis = _gizmo_rotation * float3{0.0f, 1.0f, 0.0f};
-      const float3 z_axis = _gizmo_rotation * float3{0.0f, 0.0f, 1.0f};
-
-      if (_translate.translating) {
-         switch (_translate.active_axis) {
-         case axis::x: {
-            tool_visualizers.add_line_overlay(_gizmo_position - x_axis * _axis_line_length,
-                                              _gizmo_position + x_axis * _axis_line_length,
-                                              x_color);
-         } break;
-         case axis::y: {
-            tool_visualizers.add_line_overlay(_gizmo_position - y_axis * _axis_line_length,
-                                              _gizmo_position + y_axis * _axis_line_length,
-                                              y_color);
-         } break;
-         case axis::z: {
-            tool_visualizers.add_line_overlay(_gizmo_position - z_axis * _axis_line_length,
-                                              _gizmo_position + z_axis * _axis_line_length,
-                                              z_color);
-         } break;
-         }
-      }
-
-      tool_visualizers.add_line_overlay(_gizmo_position,
-                                        _gizmo_position + x_axis * length,
-                                        x_hover ? x_color_hover : x_color);
-      tool_visualizers.add_line_overlay(_gizmo_position,
-                                        _gizmo_position + y_axis * length,
-                                        y_hover ? y_color_hover : y_color);
-      tool_visualizers.add_line_overlay(_gizmo_position,
-                                        _gizmo_position + z_axis * length,
-                                        z_hover ? z_color_hover : z_color);
-   }
-   else if (_mode == mode::rotate) {
+   if (_mode == mode::rotate) {
       constexpr float pi_2 = std::numbers::pi_v<float> * 2.0f;
       constexpr float divisor = circle_divisions;
       const float radius = _rotate_gizmo_radius;
@@ -374,68 +251,6 @@ void gizmo::draw(world::tool_visualizers& tool_visualizers) noexcept
    }
 }
 
-void gizmo::draw_active_axis_line(const quaternion rotation, const float3 positionWS,
-                                  world::tool_visualizers& tool_visualizers)
-{
-   if (_translate.translating) {
-      switch (_translate.active_axis) {
-      case axis::x: {
-         const float3 x_axis = rotation * float3{1.0f, 0.0f, 0.0f};
-
-         tool_visualizers.add_line(positionWS - x_axis * _axis_line_length,
-                                   positionWS + x_axis * _axis_line_length,
-                                   x_color & 0x60'ff'ff'ffu);
-      } break;
-      case axis::y: {
-         const float3 y_axis = rotation * float3{0.0f, 1.0f, 0.0f};
-
-         tool_visualizers.add_line(positionWS - y_axis * _axis_line_length,
-                                   positionWS + y_axis * _axis_line_length,
-                                   y_color & 0x60'ff'ff'ffu);
-      } break;
-      case axis::z: {
-         const float3 z_axis = rotation * float3{0.0f, 0.0f, 1.0f};
-
-         tool_visualizers.add_line(positionWS - z_axis * _axis_line_length,
-                                   positionWS + z_axis * _axis_line_length,
-                                   z_color & 0x60'ff'ff'ffu);
-      } break;
-      }
-   }
-}
-
-bool gizmo::show_translate(const float3 gizmo_position,
-                           const quaternion gizmo_rotation, float3& movement) noexcept
-{
-   _gizmo_position = gizmo_position;
-   _gizmo_rotation = normalize(gizmo_rotation);
-   _used_last_tick = true;
-
-   if (std::exchange(_mode, mode::translate) != mode::translate) {
-      return false;
-   }
-
-   const float3 start_movement = movement;
-
-   if (std::optional<float> movement_x =
-          std::exchange(_translate.movement_x, std::nullopt);
-       movement_x) {
-      movement.x += *movement_x;
-   }
-   if (std::optional<float> movement_y =
-          std::exchange(_translate.movement_y, std::nullopt);
-       movement_y) {
-      movement.y += *movement_y;
-   }
-   if (std::optional<float> movement_z =
-          std::exchange(_translate.movement_z, std::nullopt);
-       movement_z) {
-      movement.z += *movement_z;
-   }
-
-   return start_movement != movement;
-}
-
 bool gizmo::show_rotate(const float3 gizmo_position,
                         const quaternion gizmo_rotation, float3& rotation) noexcept
 {
@@ -476,7 +291,6 @@ bool gizmo::show_rotate(const float3 gizmo_position,
 
 bool gizmo::can_close_last_edit() const noexcept
 {
-   if (_translating_last_frame and not _translate.translating) return true;
    if (_rotating_last_frame and not _rotate.rotating) return true;
 
    return false;
@@ -486,45 +300,6 @@ void gizmo::deactivate() noexcept
 {
    _mode = mode::inactive;
    _used_last_tick = false;
-}
-
-auto gizmo::get_translate_position(graphics::camera_ray world_ray,
-                                   const float3 camera_position,
-                                   const float3 fallback) const noexcept -> float3
-{
-   graphics::camera_ray ray;
-
-   ray.origin = conjugate(_gizmo_rotation) * (world_ray.origin);
-   ray.direction = conjugate(_gizmo_rotation) * world_ray.direction;
-
-   const float3 eye_direction =
-      conjugate(_gizmo_rotation) * (_translate.start_position - camera_position);
-   const float3 axis = [&] {
-      if (_translate.active_axis == axis::x) {
-         return float3{1.0f, 0.0f, 0.0f};
-      }
-      else if (_translate.active_axis == axis::y) {
-         return float3{0.0f, 1.0f, 0.0f};
-      }
-      else if (_translate.active_axis == axis::z) {
-         return float3{0.0f, 0.0f, 1.0f};
-      }
-
-      return float3{};
-   }();
-
-   const float3 plane_tangent = cross(axis, eye_direction);
-   const float3 plane_normal = cross(axis, plane_tangent);
-
-   const float4 plane =
-      make_plane_from_point(conjugate(_gizmo_rotation) * _translate.start_position,
-                            plane_normal);
-
-   if (const float hit = intersect_plane(ray.origin, ray.direction, plane); hit > 0.0f) {
-      return ray.origin + ray.direction * hit;
-   }
-
-   return fallback;
 }
 
 auto gizmo::get_rotate_position(const graphics::camera_ray rayGS,
--- src/gizmo.hpp
+++ src/gizmo.hpp
@@ -19,12 +19,6 @@ struct gizmo {
 
    void draw(world::tool_visualizers& tool_visualizers) noexcept;
 
-   void draw_active_axis_line(const quaternion rotation, const float3 positionWS,
-                              world::tool_visualizers& tool_visualizers);
-
-   bool show_translate(const float3 gizmo_position,
-                       const quaternion gizmo_rotation, float3& movement) noexcept;
-
    bool show_rotate(const float3 gizmo_position,
                     const quaternion gizmo_rotation, float3& rotation) noexcept;
 
@@ -33,14 +27,10 @@ struct gizmo {
    void deactivate() noexcept;
 
 private:
-   auto get_translate_position(const graphics::camera_ray world_ray,
-                               const float3 camera_position,
-                               const float3 fallback) const noexcept -> float3;
-
    auto get_rotate_position(const graphics::camera_ray rayGS,
                             const float3 fallbackGS) const noexcept -> float3;
 
-   enum class mode : uint8 { inactive, translate, rotate };
+   enum class mode : uint8 { inactive, rotate };
    enum class axis : uint8 { none, x, y, z };
 
    mode _last_mode = mode::inactive;
@@ -54,18 +44,6 @@ private:
    float4x4 _gizmo_to_world;
    float4x4 _world_to_gizmo;
 
-   struct translate_state {
-      float3 start_position = {0.0f, 0.0f, 0.0f};
-      float3 current_cursor_position = {0.0f, 0.0f, 0.0f};
-      std::optional<float> movement_x;
-      std::optional<float> movement_y;
-      std::optional<float> movement_z;
-
-      axis active_axis = axis::none;
-      bool mouse_down_outside_gizmo = false;
-      bool translating = false;
-   } _translate;
-
    struct rotate_state {
       float3 current_cursor_positionGS = {0.0f, 0.0f, 0.0f};
       std::optional<float> angle_x;
@@ -77,11 +55,8 @@ private:
       bool rotating = false;
    } _rotate;
 
-   bool _translating_last_frame = false;
    bool _rotating_last_frame = false;
 
-   float _translate_gizmo_length = 4.0f;
-   float _translate_gizmo_hit_pad = 0.25f;
    float _rotate_gizmo_radius = 4.0f;
    float _rotate_gizmo_hit_pad = 0.25f;
    float _axis_line_length = 1024.0f;
