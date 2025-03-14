454449e8caf7e78be1a0ab5108613f578452a83e
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2025-03-14 14:39:33 +1300

    shadow drawing uses same camera cb layout

--- src/graphics/renderer.cpp
+++ src/graphics/renderer.cpp
@@ -4,6 +4,7 @@
 #include "async/thread_pool.hpp"
 #include "blocks.hpp"
 #include "camera.hpp"
+#include "constant_buffers.hpp"
 #include "copy_command_list_pool.hpp"
 #include "cull_objects.hpp"
 #include "dynamic_buffer_allocator.hpp"
@@ -45,40 +46,6 @@
 #include <imgui.h>
 
 namespace we::graphics {
-
-namespace {
-
-// TODO: Put this somewhere.
-struct alignas(256) frame_constant_buffer {
-   float4x4 projection_from_world;
-   float4x4 projection_from_view;
-
-   float3 view_positionWS;
-   float texture_scroll_duration;
-
-   float2 viewport_size;
-   float2 viewport_topleft;
-
-   float line_width;
-};
-
-static_assert(sizeof(frame_constant_buffer) == 256);
-
-// TODO: Put this somewhere.
-struct alignas(256) wireframe_constant_buffer {
-   float3 color;
-};
-
-static_assert(sizeof(wireframe_constant_buffer) == 256);
-
-struct alignas(256) meta_outlined_constant_buffer {
-   float4 color;
-   float4 outline_color;
-};
-
-static_assert(sizeof(meta_outlined_constant_buffer) == 256);
-
-}
 
 struct renderer_impl final : renderer {
    renderer_impl(const renderer_init& init);
