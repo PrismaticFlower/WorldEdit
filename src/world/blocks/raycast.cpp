fe6266fd8fe2bb6804f46070452f85b05663fe3e
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2025-03-25 10:38:45 +1300

    blocks raycast returns hit surface index

--- src/world/blocks/raycast.cpp
+++ src/world/blocks/raycast.cpp
@@ -1,4 +1,5 @@
 #include "raycast.hpp"
+#include "mesh_geometry.hpp"
 
 #include "math/iq_intersectors.hpp"
 #include "math/quaternion_funcs.hpp"
@@ -31,10 +32,45 @@ auto raycast(const float3 ray_originWS, const float3 ray_directionWS,
       }
    }
 
-   return closest_index != UINT32_MAX
-             ? std::optional{raycast_block_result{.distance = closest,
-                                                  .index = closest_index}}
-             : std::nullopt;
+   if (closest_index != UINT32_MAX) {
+      const block_description_box& box = boxes.description[closest_index];
+
+      const float4x4 scale = {
+         {box.size.x, 0.0f, 0.0f, 0.0f},
+         {0.0f, box.size.y, 0.0f, 0.0f},
+         {0.0f, 0.0f, box.size.z, 0.0f},
+         {0.0f, 0.0f, 0.0f, 1.0f},
+      };
+      const float4x4 rotation = to_matrix(box.rotation);
+
+      float4x4 world_from_object = rotation * scale;
+      world_from_object[3] = {box.position, 1.0f};
+
+      uint32 surface_index = UINT32_MAX;
+      float closest_surface = FLT_MAX;
+
+      for (const std::array<uint16, 3>& tri : block_cube_triangles) {
+         const float3 pos0WS = world_from_object * block_cube_vertices[tri[0]].position;
+         const float3 pos1WS = world_from_object * block_cube_vertices[tri[1]].position;
+         const float3 pos2WS = world_from_object * block_cube_vertices[tri[2]].position;
+
+         const float3 hit =
+            triIntersect(ray_originWS, ray_directionWS, pos0WS, pos1WS, pos2WS);
+
+         if (hit.x >= 0.0f and hit.x < closest_surface) {
+            surface_index = block_cube_vertices[tri[0]].surface_index;
+            closest_surface = hit.x;
+         }
+      }
+
+      if (surface_index != UINT32_MAX) {
+         return raycast_block_result{.distance = closest,
+                                     .index = closest_index,
+                                     .surface_index = surface_index};
+      }
+   }
+
+   return std::nullopt;
 }
 
 }
--- src/world/blocks/raycast.hpp
+++ src/world/blocks/raycast.hpp
@@ -9,6 +9,7 @@ namespace we::world {
 struct raycast_block_result {
    float distance = 0.0f;
    uint32 index = 0;
+   uint32 surface_index = 0;
 };
 
 auto raycast(const float3 ray_originWS, const float3 ray_directionWS,
