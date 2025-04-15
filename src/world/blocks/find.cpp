2d3d8b8afe9bbec3950bdb3b831d95349e9b6b96
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2025-04-16 10:26:15 +1200

    add placing blocks from entity groups

--- src/world/blocks/find.cpp
+++ src/world/blocks/find.cpp
@@ -1,5 +1,7 @@
 #include "find.hpp"
 
+#include "utility/string_icompare.hpp"
+
 #pragma warning(default : 4061) // enumerator 'identifier' in switch of enum 'enumeration' is not explicitly handled by a case label
 #pragma warning(default : 4062) // enumerator 'identifier' in switch of enum 'enumeration' is not handled
 
@@ -31,6 +33,48 @@ auto find_block(const blocks& blocks, const block_id id) -> std::optional<uint32
    }
 
    std::unreachable();
+}
+
+auto find_block_equivalent_material(const blocks& blocks, const block_material& material)
+   -> std::optional<uint8>
+{
+   using string::iequals;
+
+   for (uint32 material_index = 0; material_index < world::max_block_materials;
+        ++material_index) {
+      const block_material& other = blocks.materials[material_index];
+
+      if (not iequals(other.diffuse_map, material.diffuse_map)) continue;
+      if (not iequals(other.normal_map, material.normal_map)) continue;
+      if (not iequals(other.detail_map, material.detail_map)) continue;
+      if (not iequals(other.env_map, material.env_map)) continue;
+      if (other.detail_tiling != material.detail_tiling) continue;
+      if (other.tile_normal_map != material.tile_normal_map) continue;
+      if (other.specular_lighting != material.specular_lighting) continue;
+      if (other.specular_lighting != material.specular_lighting) continue;
+
+      return static_cast<uint8>(material_index);
+   }
+
+   return std::nullopt;
+}
+
+auto find_block_empty_material(const blocks& blocks) -> std::optional<uint8>
+{
+   using string::iequals;
+
+   const block_material empty_material;
+
+   for (uint32 material_index = 0; material_index < world::max_block_materials;
+        ++material_index) {
+      const block_material& other = blocks.materials[material_index];
+
+      if (other != empty_material) continue;
+
+      return static_cast<uint8>(material_index);
+   }
+
+   return std::nullopt;
 }
 
 }
