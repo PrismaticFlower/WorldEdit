19bfe48530ffd1ce7118608e75c02e21c3a210fe
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2025-11-04 09:48:15 +1300

    fix typo

--- src/world/utility/double_click_select.hpp
+++ src/world/utility/double_click_select.hpp
@@ -10,17 +10,7 @@ namespace we::world {
 struct blocks_custom_mesh_bvh_library;
 struct object_class_library;
 
-enum class double_click_select_op { add, remove };
-
-struct double_click_settings {
-   float path_node_radius = 0.0f;
-   float barrier_visualizer_height = 0.0f;
-   float hub_visualizer_height = 0.0f;
-   float connection_visualizer_height = 0.0f;
-   float boundary_visualizer_height = 0.0f;
-};
-
-/// @brief Add (or remove) blocks that intersect the frustum to (or from) the selection.
+/// @brief Add (or remove) similar entities that intersect the frustum to (or from) the selection.
 /// @param hovered_entity The hovered entity to use for similarity.
 /// @param world The world.
 /// @param object_classes The object classes.
